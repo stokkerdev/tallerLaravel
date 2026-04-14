fa4a3ab432d7807b1de3f192e0f91266cd1fd884
Stokker <stokkerma@gmail.com>
2026-04-14 18:29:20 -0500

    Relacion de modelos con eloquent

--- app/Models/car.php
+++ app/Models/car.php
@@ -4,19 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\category;
+
+
 
 class car extends Model
 {
 
-use HasFactory;
-protected $fillable = [
-    'car_make',
-    'car_model',
-    'car_year',
-    'car_price',
-    'car_status',
-    'fk_categoria_id',
-    'barcode'
-];
-    
+    use HasFactory;
+    protected $fillable = [
+        'car_make',
+        'car_model',
+        'car_year',
+        'car_price',
+        'car_status',
+        'fk_categoria_id',
+        'barcode'
+    ];
+
+    function category()
+    {
+        return $this->belongsTo(category::class, 'fk_categoria_id');
+    }
+
 }
